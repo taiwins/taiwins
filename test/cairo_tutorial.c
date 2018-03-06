3e0a34f7959b043d36b8898225558d5c19077475
Xichen Zhou <xzhou@xeechou.net>
2018-03-06 17:41:46 -0500

    clean cairo surface by cairo_rectangle

--- test/cairo_tutorial.c
+++ test/cairo_tutorial.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <librsvg/rsvg.h>
@@ -7,7 +9,7 @@
 //we try out several painting patterns then
 cairo_surface_t * rendertext(const char *text)
 {
-	cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 50, 16);
+	cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 8 * strlen(text), 16);
 	cairo_t *cr = cairo_create(surface);
 	cairo_text_extents_t extent;
 	cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 1.0);
@@ -17,7 +19,7 @@ cairo_surface_t * rendertext(const char *text)
 	fprintf(stderr, "text extends (%f, %f)\n", extent.height, extent.width);
 	cairo_move_to(cr, 25 - extent.width /2, 8 + extent.height/2);
 	cairo_show_text(cr, text);
-	cairo_surface_write_to_png(surface, "/tmp/debug1.png");
+//	cairo_surface_write_to_png(surface, "/tmp/debug1.png");
 	cairo_destroy(cr);
 	return surface;
 }
@@ -34,9 +36,14 @@ int main(int argc, char *argv[])
 	cairo_surface_t *target = cairo_image_surface_create_for_data(content, format, width, height, stride);
 	cairo_surface_t *font = rendertext("text");
 	cairo_t *cr = cairo_create(target);
-//	cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0);
-//	cairo_paint(cr);
 	cairo_set_source_surface(cr, font, 20, 100);
+	cairo_paint(cr);
+	cairo_rectangle(cr, 20, 100, 50, 16);
+	//it doesn't override the the content if you use 0 on alpha
+	cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 0.0);
+	cairo_paint(cr);
+	cairo_surface_t *another_text = rendertext("another");
+	cairo_set_source_surface(cr, another_text, 20, 100);
 	cairo_paint(cr);
 	cairo_surface_write_to_png(target, "/tmp/debug.png");
 	cairo_surface_destroy(target);
