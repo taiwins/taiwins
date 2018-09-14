23baa9cc17e78343b2abefdd1c37b7b5f216c01a
Xichen Zhou <xzhou@xeechou.net>
2018-09-14 16:09:36 -0400

    workaround to have nvidia happy

--- clients/nk_wl_egl.c
+++ clients/nk_wl_egl.c
@@ -167,6 +167,8 @@ assign_egl_surface(struct app_surface *app_surface, const struct egl_env *env)
 	assert(app_surface->eglsurface);
 	assert(eglMakeCurrent(env->egl_display, app_surface->eglsurface,
 			      app_surface->eglsurface, env->egl_context));
+	//TODO I am getting GL_INVALID_OPERATION on this, but eglMakeCurrent succeed.
+	glGetError();
 	glViewport(0, 0, app_surface->w, app_surface->h);
 	glScissor(0, 0, app_surface->w, app_surface->h);
 	/* EGLint value; */
