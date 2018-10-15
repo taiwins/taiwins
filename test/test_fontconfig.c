99c1b43ba9f8c459873764b93c6cc21eaf1ebe7c
Xichen Zhou <xzhou@xeechou.net>
2018-10-14 21:54:30 -0400

    remove unreleated

--- test/test_fontconfig.c
+++ test/test_fontconfig.c
@@ -66,73 +66,79 @@ union_unicode_range(const nk_rune left[2], const nk_rune right[2], nk_rune out[2
 }
 
 //return true if left and right are insersected, else false
-bool
+static bool
 intersect_unicode_range(const nk_rune left[2], const nk_rune right[2])
 {
 	return (left[0] <= right[1] && left[1] >= right[1]) ||
 		(left[0] <= right[0] && left[1] >= right[0]);
 }
 
+static int
+unicode_range_compare(const void *l, const void *r)
+{
+	const nk_rune *range_left = (const nk_rune *)l;
+	const nk_rune *range_right = (const nk_rune *)r;
+	return ((int)range_left[0] - (int)range_right[0]);
+}
 
+//we can only merge one range at a time
 static int
 merge_unicode_range(const nk_rune *left, const nk_rune *right, nk_rune *out)
 {
-	//this is not sufficient, we need a union method
+	//get the range
 	int left_size = 0;
 	while(*(left+left_size)) left_size++;
 	int right_size = 0;
 	while(*(right+right_size)) right_size++;
-
-	nk_rune merged[left_size+right_size];
-	int i = 0; int j = 0; int m = 0;
-	merged[0] = (left[0] < right[0]) ? left[0] : right[0];
-	merged[1] = (left[0] < right[0]) ? left[1] : right[1];
-	//you need to get the next and be able to increase it
-	while (i < (left_size/2) || j < (right_size/2)) {
-		const nk_rune *next = (left[i*2] < right[j*2]) ? &left[i*2] : &right[j*2];
-		int *next_id = (left[i*2] < right[j*2]) ? &i : &j;
-		//if nothing is intersected, we have to move forward and copy
-		//one to merged
-		if (!intersect_unicode_range(&merged[m*2], next)) {
+	//sort the range,
+	nk_rune sorted_ranges[left_size+right_size];
+	memcpy(sorted_ranges, left, sizeof(nk_rune) * left_size);
+	memcpy(sorted_ranges+left_size, right, sizeof(nk_rune) * right_size);
+	qsort(sorted_ranges, (left_size+right_size)/2, sizeof(nk_rune) * 2,
+	      unicode_range_compare);
+	//merge algorithm
+	nk_rune merged[left_size+right_size+1];
+	merged[0] = sorted_ranges[0];
+	merged[1] = sorted_ranges[1];
+	int m = 0;
+	for (int i = 0; i < (left_size+right_size) / 2; i++) {
+		if (intersect_unicode_range(&sorted_ranges[i*2],
+					    &merged[2*m]))
+			union_unicode_range(&sorted_ranges[i*2], &merged[2*m],
+					    &merged[2*m]);
+		else {
 			m++;
-			merged[2*m] = next[0];
-			merged[2*m+1] = next[1];
-			*next_id += 1;
-		} else {
-			//otherwise we do the merge
-			union_unicode_range(next, &merged[m], &merged[m]);
-			*next_id += 1;
+			merged[2*m] = sorted_ranges[2*i];
+			merged[2*m+1] = sorted_ranges[2*i+1];
 		}
 	}
-	for (int i = 0; i< m; i++)
-		fprintf(stderr, "(%x, %x),\n", merged[2*i], merged[2*i+1]);
+	m++;
+	merged[2*m] = 0;
 
 	if (!out)
 		return 2*m;
-	memcpy(out, left, sizeof(nk_rune) * left_size);
-	memcpy(out + left_size, right, sizeof(nk_rune) * right_size);
-	*(out+left_size+right_size) = 0;
-	return left_size + right_size;
+	memcpy(out, merged, (2*m+1) * sizeof(nk_rune));
+	return 2*m;
 }
 
-
 static void
-save_the_font_images(void)
+save_the_font_images(const nk_rune *glyph_range)
 {
 	int w, h;
 	struct nk_font_config cfg = nk_font_config(16);
-	cfg.merge_mode = nk_true;
-	cfg.range = nk_font_chinese_glyph_ranges();
-	cfg.coord_type = NK_COORD_UV;
+	cfg.range = glyph_range;
 
 	struct nk_font_atlas atlas;
 	nk_font_atlas_init_default(&atlas);
 	nk_font_atlas_begin(&atlas);
-
-	nk_font_atlas_add_from_file(&atlas,
-				    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
-				    16, 0);
+//	nk_font_atlas_add_default(&atlas, 16.0, &cfg);
+	//this does not work dude
+	struct nk_font * font = nk_font_atlas_add_from_file(
+		&atlas,
+		"/usr/share/fonts/TTF/Inconsolata-Regular.ttf",
+		16, &cfg);
 	const void *data = nk_font_atlas_bake(&atlas, &w, &h, NK_FONT_ATLAS_ALPHA8);
+
 	printf("we had a texture of size %d, %d\n", w, h);
 	//this may not be right...
 	cairo_surface_t *image_surface =
@@ -140,8 +146,11 @@ save_the_font_images(void)
 						    CAIRO_FORMAT_A8,
 						    w, h,
 						    cairo_format_stride_for_width(CAIRO_FORMAT_A8, w));
+
 	cairo_surface_write_to_png(image_surface, "/tmp/save_as_png.png");
 	cairo_surface_destroy(image_surface);
+	nk_font_atlas_end(&atlas, nk_handle_id(0), NULL);
+	//I will need to write a allocator for nuklear though, this sucks.
 }
 
 int
@@ -149,8 +158,8 @@ main(int argc, char *argv[])
 {
 	FcInit();
 	FcConfig* config = FcConfigGetCurrent();
-	FcFontSet* sys_fonts = FcConfigGetFonts(config, FcSetSystem);
-	FcFontSet* app_fonts = FcConfigGetFonts(config, FcSetApplication);
+	/* FcFontSet* sys_fonts = FcConfigGetFonts(config, FcSetSystem); */
+	/* FcFontSet* app_fonts = FcConfigGetFonts(config, FcSetApplication); */
 	//make pattern from font name
 	/* FcPattern* pat = FcNameParse((const FcChar8*)"Font Awesome 5 Free"); */
 	FcPattern* pat = FcNameParse((const FcChar8*)"DejaVu Sans");
@@ -183,9 +192,16 @@ main(int argc, char *argv[])
 	/* FcFontSetDestroy(app_fonts); */
 	FcConfigDestroy(config);
 	FcFini();
-	save_the_font_images();
-	const nk_rune* crange = nk_font_chinese_glyph_ranges();
-	const nk_rune* krange = nk_font_korean_glyph_ranges();
-	printf("%d\n", merge_unicode_range(crange, krange, NULL));
+
+	int total_range  = merge_unicode_range(nk_font_chinese_glyph_ranges(),
+					       nk_font_korean_glyph_ranges(), NULL);
+	nk_rune rune_range[total_range+1];
+	merge_unicode_range(nk_font_chinese_glyph_ranges(),
+			    nk_font_korean_glyph_ranges(), rune_range);
+	for (int i = 0; i < total_range/2; i++) {
+		printf("(%x, %x), ", rune_range[i*2], rune_range[i*2+1]);
+	}
+	printf("%x\n", rune_range[total_range]);
+	save_the_font_images(rune_range);
 	return 0;
 }
