ed2426a8e3db5d68369742d0947d3be2ef97bf61
sichem <xeechou@gmail.com>
2018-12-13 16:16:07 -0500

    font rendering using masking

--- test/cairo_tutorial.c
+++ test/cairo_tutorial.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdlib.h>
@@ -52,101 +53,112 @@ static void cairo_clean(cairo_t *context, double x, double y, double w, double h
 	cairo_paint(context);
 }
 
-
-//we try out several painting patterns then
-static cairo_surface_t *
-rendertext_with_caret(const char *text, int font_size)
+void print_bitmap(unsigned char *bitmap, int width, int height, int pitch)
 {
-	int pix_size = font_pt2px(font_size, 96);
-	//it is probably bigger
-	cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 16 * strlen(text), pix_size);
-	cairo_t *cr = cairo_create(surface);
-	cairo_set_source_rgba(cr, 0, 0, 0, 1.0);
-	cairo_paint(cr);
-	cairo_text_extents_t extent;
-	cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0);
-	cairo_select_font_face(cr, "sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-	cairo_set_font_size(cr, 16);
-	cairo_text_extents(cr, text, &extent);
-	fprintf(stderr, "text extends (%f, %f), and surface is  (%ld, %d)\n",
-		extent.width, extent.height, 16 * strlen(text), pix_size);
-//	assert(extent.height == (double)pix_size);
-	//I think they draw on that point
-	cairo_move_to(cr, 0, extent.height);
-	cairo_show_text(cr, text);
-	cairo_move_to(cr, extent.width + 6.0, 0);
-	cairo_line_to(cr, extent.width + 6.0, extent.height);
-	cairo_set_line_width(cr, 6.0);
-	cairo_stroke(cr);
-//	cairo_surface_write_to_png(surface, "/tmp/debug1.png");
-	cairo_destroy(cr);
-	return surface;
+	char row[width+1];
+	row[width] = '\0';
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++)
+			row[j] = (bitmap[pitch * i + j] != 0) ? 'A' : ' ';
+		fprintf(stderr, "%s\n", row);
+	}
 }
-
 
 /* luckly we know that cairo text extends */
 int main(int argc, char *argv[])
 {
+	int ft_err;
+	int glyph_index;
 	//create canvas and paint clean it
 	int width = 400;
 	int height = 200;
 	cairo_format_t format = CAIRO_FORMAT_ARGB32;
 	FT_Library library;
-	FT_Face face;
-	const cairo_user_data_key_t key;
+	FT_Face font_face;
 
-	FT_Init_FreeType(&library);
+	ft_err = FT_Init_FreeType(&library);
+	FT_New_Face(library, "/usr/share/fonts/TTF/Vera.ttf", 0, &font_face);
+	FT_Set_Pixel_Sizes(font_face, 0, 16);
+	glyph_index = FT_Get_Char_Index(font_face, 'M');
+	FT_Load_Glyph(font_face, glyph_index, FT_LOAD_DEFAULT);
+	FT_Render_Glyph(font_face->glyph, FT_RENDER_MODE_NORMAL);
+	FT_Bitmap *bitmap = &font_face->glyph->bitmap;
+	assert(bitmap->pixel_mode == FT_PIXEL_MODE_GRAY);
+	int glyph_width = bitmap->width;
+	int glyph_height = bitmap->rows;
+	int glyph_stride = bitmap->pitch;
+	uint32_t pixels[glyph_width * glyph_height];
+	for (int i = 0; i< glyph_width * glyph_height; i++) {
+		pixels[i] = bitmap->buffer[i] << 24;
+	}
 
+	cairo_surface_t *glyph_surface =
+		cairo_image_surface_create_for_data(pixels, CAIRO_FORMAT_ARGB32,
+						    glyph_width, glyph_height,
+						    cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, glyph_width));
+	print_bitmap(bitmap->buffer, glyph_width, glyph_height, glyph_stride);
+
+	/* fprintf(stderr, "glyph width: %d, height:%d\n", glyph_width, glyph_height); */
+	/* cairo_pattern_t *glyph_pattern = cairo_pattern_create_for_surface(glyph_surface); */
+	/* cairo_surface_write_to_png(glyph_surface, "/tmp/glyph.png"); */
+
+
+
+	//I should create the
 	int stride = cairo_format_stride_for_width(format, width);
 	unsigned char content[height * stride];
 	memset(content, 255, height * stride);
 	cairo_surface_t *surface = cairo_image_surface_create_for_data(content, format, width, height, stride);
+	cairo_matrix_t matrix;
+
+
+	/* cairo_pattern_type_t p_type = cairo_pattern_get_type(p); */
+	/* fprintf(stderr, "the pattern type is %d\n", p_type); */
+
+	fprintf(stderr, "matrix scale (%f, %f), shift: (%f, %f)\n",
+		matrix.xx, matrix.yy, matrix.x0, matrix.y0);
+
 	cairo_t *cr = cairo_create(surface);
-	int error = FT_New_Face(library,
-				"/usr/share/fonts/truetype/fa-regular-400.ttf",
-				0, &face);
-	cairo_font_face_t *font_face = cairo_ft_font_face_create_for_ft_face(face, 0);
-	fprintf(stderr, "cairo_font face: %p\n", font_face);
-	if (cairo_font_face_status(font_face) != CAIRO_STATUS_SUCCESS)
-		fprintf(stderr, "oh shit, I screwed something up\n");
-	cairo_set_font_face(cr, font_face);
-	cairo_set_font_size(cr, 20);
-	cairo_scaled_font_t *scaled_font = cairo_get_scaled_font(cr);
-	if (cairo_scaled_font_status(scaled_font) != CAIRO_STATUS_SUCCESS)
-		fprintf(stderr, "oh shit, scaled font didn\'t work\n");
 
-	cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
-	cairo_paint(cr);
-	unsigned int unicodes[] = {0xF1C1, 0xF1C2, 0xF1C3, 0xF1C4, 0xF1C5, 0xF1C6};
-	char text[256];
-	int count = 0;
-	for (int i = 0; i < 6; i++) {
-		count += nk_utf_encode(unicodes[i], text+count, 256 - count);
-	}
-	text[count] = '\0';
+	cairo_pattern_t *p = cairo_pattern_create_for_surface(glyph_surface);
+	cairo_pattern_type_t p_type = cairo_pattern_get_type(p);
+	fprintf(stderr, "the pattern type is %d\n", p_type);
 
-//	char text[] = "we should see a cursor at the end.M";
-	cairo_move_to(cr, 100, 100);
+	cairo_matrix_init_identity (&matrix);
+	cairo_matrix_scale (&matrix, glyph_width, glyph_height);
+	cairo_pattern_set_matrix (p, &matrix);
+
+	/* cairo_save(cr); */
+	cairo_translate(cr, 100, 50);
+	cairo_scale(cr, glyph_width, glyph_height);
+	/* cairo_rectangle(cr, 10, 10, 100, 100); */
 	cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
-	{
-		int num_glyphs;
-		cairo_glyph_t *glyphs;
-		int num_clusters;
-		cairo_text_cluster_t *clusters;
-		cairo_text_cluster_flags_t flags;
-		cairo_font_extents_t extents;
-		cairo_scaled_font_extents(scaled_font, &extents);
-		fprintf(stderr, "%d\n", extents.ascent);
-		cairo_scaled_font_text_to_glyphs(scaled_font, 100, 100+extents.ascent,
-						 text, count, &glyphs, &num_glyphs, &clusters, &num_clusters, &flags);
-		cairo_show_text_glyphs(cr, text, count, glyphs, num_glyphs, clusters, num_clusters, flags);
+	cairo_pattern_t *linpat = cairo_pattern_create_linear(0, 0, 1, 1);
+	cairo_pattern_add_color_stop_rgb(linpat, 0, 0.3, 0.3, 0.8);
+	cairo_pattern_add_color_stop_rgb(linpat, 1, 0.0, 0.8, 0.3);
+	/* cairo_fill(cr); */
 
-		/* cairo_glyph_free(glyphs); */
-		/* cairo_text_cluster_free(clusters); */
-	}
+	/* cairo_pattern_t *radpat = cairo_pattern_create_radial(0.5, 0.5, 0.25, 0.5, 0.5, 0.75); */
+	/* cairo_pattern_add_color_stop_rgba (radpat, 0, 0, 0, 0, 1); */
+	/* cairo_pattern_add_color_stop_rgba (radpat, 0.5, 0, 0, 0, 0); */
+	cairo_set_source(cr, linpat);
+	/* cairo_rectangle(cr, 0, 0, 1, 1); */
+	cairo_mask(cr, p);
+	/* cairo_fill(cr); */
+	/* cairo_mask_surface(cr, glyph_surface, 0.0, 0.0); */
+	//rectangle is a type of pattern as well,
+	/* cairo_rectangle(cr, 0.0, 0.0, 1.0, 1.0); */
+	/* cairo_paint(cr); */
+	/* cairo_mask(cr, glyph_pattern); */
+	/* cairo_restore(cr); */
+
 	cairo_surface_write_to_png(surface, "/tmp/debug.png");
 	cairo_destroy(cr);
 //	cairo_surface_destroy(font_surf);
 
 	cairo_surface_destroy(surface);
+
+	FT_Done_Face(font_face);
+	FT_Done_FreeType(library);
+
 }
