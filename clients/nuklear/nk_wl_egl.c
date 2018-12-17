e1f4241620127ee1750a408935d305277706099c
sichem <xeechou@gmail.com>
2018-12-17 15:25:14 -0500

    nvidia end bug detect

--- clients/nuklear/nk_wl_egl.c
+++ clients/nuklear/nk_wl_egl.c
@@ -363,7 +363,7 @@ _nk_egl_draw_begin(struct nk_egl_backend *bkend,
 	glBindVertexArray(bkend->vao);
 	glBindBuffer(GL_ARRAY_BUFFER, bkend->vbo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bkend->ebo);
-
+	assert(glGetError() == GL_NO_ERROR);
 	//I guess it is not really a good idea to allocate buffer every frame.
 	//if we already have the glBufferData, we would just mapbuffer
 	vertices = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
