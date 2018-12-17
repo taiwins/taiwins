9c781b7ba7bbe9129917381c35a5116fa31e1533
Sichem Zhou <xeechou@gmail.com>
2018-12-16 22:20:39 -0500

    move icons into string

--- clients/widget.c
+++ clients/widget.c
@@ -8,21 +8,13 @@ static int
 redraw_panel(void *data)
 {
 	struct app_surface *ancre = data;
-	//this should call the
+	//panel gets redrawed for once we have a event
 	ancre->do_frame(ancre, 0);
 	return true;
 }
 
 
-void
-shell_widget_init_ancre(struct shell_widget *widget, struct app_surface *panel,
-			uint32_t w, uint32_t h, uint32_t px, uint32_t py)
-{
-	embeded_impl_app_surface(&widget->ancre, panel, w, h, px, py);
-	//now the draw function is set.
-}
-
-void
+static void
 shell_widget_event_from_timer(struct shell_widget *widget, struct timespec time,
 			      struct tw_event_queue *event_queue)
 {
@@ -34,8 +26,9 @@ shell_widget_event_from_timer(struct shell_widget *widget, struct timespec time,
 }
 
 
-void shell_widget_event_from_file(struct shell_widget *widget, const char *path,
-				  struct tw_event_queue *event_queue)
+static void
+shell_widget_event_from_file(struct shell_widget *widget, const char *path,
+			     struct tw_event_queue *event_queue)
 {
 	int fd = 0;
 	uint32_t mask = 0;
@@ -54,7 +47,7 @@ shell_widget_activate(struct shell_widget *widget, struct app_surface *panel, st
 {
 	if (widget->interval.tv_sec || widget->interval.tv_nsec)
 		shell_widget_event_from_timer(widget, widget->interval, queue);
-	if (widget->file_path)
+	else if (widget->file_path)
 		shell_widget_event_from_file(widget, widget->file_path, queue);
 	//the size of the ancre here is irrelevant
 	embeded_impl_app_surface(&clock_widget.ancre, panel, 0, 0, 0, 0);
@@ -62,17 +55,6 @@ shell_widget_activate(struct shell_widget *widget, struct app_surface *panel, st
 
 
 
-void
-shell_widget_launch(struct shell_widget *widget, struct wl_surface *surface, struct wl_proxy *p,
-		    struct nk_wl_backend *bkend, uint32_t x, uint32_t y)
-{
-	struct wl_globals *globals = widget->widget.wl_globals;
-	app_surface_init(&widget->widget, surface, p);
-	widget->widget.wl_globals = globals;
-	nk_egl_impl_app_surface(&widget->widget, bkend, widget->draw_cb,
-				widget->w, widget->h, x, y);
-	app_surface_frame(&widget->widget, false);
-}
 
 /******************************* The sample widgets *********************************/
 
@@ -85,23 +67,19 @@ shell_widget_launch(struct shell_widget *widget, struct wl_surface *surface, str
  * one widget right now, as it is what most time is. But we may need to have
  * more versatile approach
  */
-static void
-clock_widget_anchor(struct nk_context *ctx, float width, float height, struct app_surface *app)
+static int
+clock_widget_anchor(struct shell_widget *widget, struct shell_widget_label *label)
 {
 	//it seems to work, we still need a library to figure out the text size
 	/* nk_layout_row_push(ctx, 100); */
 	static const char * daysoftheweek[] =
 		{"sun", "mon", "tus", "wed", "thu", "fri", "sat"};
-	char formatedtime[20];
 	time_t epochs = time(NULL);
 	struct tm *tim = localtime(&epochs);
 
-	sprintf(formatedtime, "%s %02d:%02d:%02d",
-		daysoftheweek[tim->tm_wday], tim->tm_hour, tim->tm_min, tim->tm_sec);
-	nk_button_label(ctx, formatedtime);
-	/* nk_label(ctx, formatedtime, NK_TEXT_CENTERED); */
+	return snprintf(label->label, 255, "%s %02d:%02d:%02d",
+		       daysoftheweek[tim->tm_wday], tim->tm_hour, tim->tm_min, tim->tm_sec);
 }
-
 
 static void
 clock_widget_sample(struct nk_context *ctx, float width, float height, struct app_surface *app)
@@ -129,8 +107,6 @@ clock_widget_sample(struct nk_context *ctx, float width, float height, struct ap
 	nk_layout_row_dynamic(ctx, 25, 1);
 	nk_edit_buffer(ctx, NK_EDIT_FIELD, &text_edit, nk_filter_default);
 }
-
-
 
 struct shell_widget clock_widget = {
 	.ancre_cb = clock_widget_anchor,
