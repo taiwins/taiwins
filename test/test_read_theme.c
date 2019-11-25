1af00b8cb3d010318ba4dbeef5c639449ea58887
Xichen Zhou <xzhou@xeechou.net>
2019-11-25 18:16:53 -0500

    test with read theme dirs

--- test/test_read_theme.c
+++ test/test_read_theme.c
@@ -6,7 +6,9 @@
 #include <ctype.h>
 #include <string.h>
 #include <strings.h>
+#include <wayland-util.h>
 
+#include <strops.h>
 #include <sequential.h>
 #include <os/file.h>
 
@@ -20,28 +22,49 @@ enum icon_section_type {
 	SEC_ICON_UNKNOWN,
 };
 
-struct icon_theme {
+#define MAX_DIR_LEN 32
+
+struct icon_dir {
+	char dir[MAX_DIR_LEN];
+	uint32_t size;
+};
+
+
+static void inline
+copy_icon_dir(struct icon_dir *dir, const uint32_t size, const char *path)
+{
+	strcpy(dir->dir, path);
+	dir->size = size;
+}
+
+struct icontheme_dir {
 	vector_t apps;
 	vector_t mimes;
 	vector_t places;
 	vector_t status;
 	vector_t devices;
+	char theme_dir[256];
 };
 
-
-static inline void
-rtrim(char *line)
+static void
+icontheme_dir_init(struct icontheme_dir *theme, const char *path)
 {
-	char *ptr = line + strlen(line) - 1;
-	while(ptr >= line && *ptr && isspace(*ptr)) *ptr-- = '\0';
+	strcpy(theme->theme_dir, path);
+	vector_init_zero(&theme->apps, sizeof(struct icon_dir), NULL);
+	vector_init_zero(&theme->mimes, sizeof(struct icon_dir), NULL);
+	vector_init_zero(&theme->places, sizeof(struct icon_dir), NULL);
+	vector_init_zero(&theme->status, sizeof(struct icon_dir), NULL);
+	vector_init_zero(&theme->devices, sizeof(struct icon_dir), NULL);
 }
 
-static inline char *
-ltrim(char *line)
+static void
+icontheme_dir_release(struct icontheme_dir *theme)
 {
-	char *ptr = line;
-	while (*ptr && isspace(*ptr)) ptr++;
-	return ptr;
+	vector_destroy(&theme->apps);
+	vector_destroy(&theme->mimes);
+	vector_destroy(&theme->places);
+	vector_destroy(&theme->status);
+	vector_destroy(&theme->devices);
 }
 
 static inline char *
@@ -64,7 +87,7 @@ is_section_name(const char *line)
 static inline enum icon_section_type
 get_section_type(const char *section)
 {
-	if (strcasestr(section, "icon theme"))
+	if (strcmp(section, "[Icon Theme]") == 0)
 		return SEC_ICON_THEME;
 	else if (strcasestr(section, "apps"))
 		return SEC_ICON_APP;
@@ -79,28 +102,54 @@ get_section_type(const char *section)
 	return SEC_ICON_UNKNOWN;
 }
 
-void
-search_icon_dir(char *path, int min_res, int max_res)
+struct size_state {
+	char currdir[MAX_DIR_LEN];
+	int curr_size, curr_max_size, curr_min_size;
+	int curr_scale;
+};
+
+static inline bool
+is_size_right(const int min_res, const int max_res,
+	      const struct size_state *curr)
 {
-	char theme_file[1000];
-	int allocated = 1000, len = 0;
+	bool inbound = INBOUND(curr->curr_size, min_res, max_res);
+	if (curr->curr_max_size == -1 || curr->curr_max_size == -1)
+		return inbound;
+	bool minmax = curr->curr_min_size >= min_res &&
+		curr->curr_max_size <= max_res;
+	return inbound && minmax;
+}
+
+static inline void
+size_state_reset(struct size_state *state, const char *path)
+{
+	strcpy(state->currdir, path);
+	state->curr_max_size = -1;
+	state->curr_min_size = -1;
+	state->curr_size = -1;
+	state->curr_scale = 1;
+}
+
+static int
+cmp_icon_dir(const void *l, const void *r)
+{
+	const struct icon_dir *ld = l, *rd = r;
+	return rd->size - ld->size;
+}
+
+void
+search_icon_dirs(struct icontheme_dir *output,
+	const char *path, const int min_res, const int max_res)
+{
+	struct size_state state = {{0}, -1, -1, -1, 1};
+        char theme_file[1000];
+	size_t allocated = 1000, len = 0;
 	char *rawline = malloc(1000);
-	char subdir[32] = {0};
-	struct icon_theme theme;
-
-	vector_init_zero(&theme.apps, 32, NULL);
-	vector_init_zero(&theme.mimes, 32, NULL);
-	vector_init_zero(&theme.places, 32, NULL);
-	vector_init_zero(&theme.status, 32, NULL);
-	vector_init_zero(&theme.devices, 32, NULL);
-
 	DIR *dir = opendir(path);
-	struct dirent *index = dir_find_pattern(dir, "index.theme");
-	if (!index)
-		goto out;
-	//read the index
+
+	icontheme_dir_init(output, path);
 	strcpy(theme_file, path);
-	path_concat(theme_file, 999, 1, index->d_name);
+	path_concat(theme_file, 999, 1, "index.theme");
 	if (!is_file_exist(theme_file))
 		goto out;
 
@@ -108,70 +157,204 @@ search_icon_dir(char *path, int min_res, int max_res)
 	enum icon_section_type curr_section = SEC_ICON_UNKNOWN;
 
 	while ((len = getline(&rawline, &allocated, file)) != -1) {
-		char *line = ltrim(rawline); rtrim(line);
+		char *line = strop_ltrim(rawline); strop_rtrim(line);
 		char *comment_point = strstr(line, "#");
 		if (comment_point)
 			*comment_point = '\0';
 
 		if (strlen(line) == 0)
 			continue;
+		//get section
 		if (is_section_name(line)) {
 			curr_section = get_section_type(line);
 			line = brace_remove(line);
-			strcpy(subdir, line);
+			//skip over the dirs that too long
+			if (strlen(line) >= MAX_DIR_LEN-1)
+				curr_section = SEC_ICON_UNKNOWN;
+			else
+				size_state_reset(&state, line);
 			continue;
 		}
-		if (curr_section != SEC_ICON_UNKNOWN)
-			break;
-
+		if (curr_section == SEC_ICON_UNKNOWN)
+			continue;
+		//theme also has the inherits attributes, it complicates the
+		//logics
 		char *equal = NULL;
 		equal = strstr(line, "=");
 		if (equal)
 			*equal = '\0';
-		int curr_size = -1, minsize = 10000, maxsize = -1000;
+
+		//deal with key value
 		char *key = line;
 		char *value = equal + 1;
-
+		//size is a required key, so we are good
 		if (strcasecmp(key, "size") == 0) {
-			curr_size = atoi(value);
+			state.curr_size = state.curr_scale * atoi(value);
 			continue;
-		} if (strcasecmp(key, "minsize") == 0) {
-			minsize = atoi(value);
+		} else if (strcasecmp(key, "minsize") == 0) {
+			state.curr_min_size = atoi(value);
 			continue;
-		} if (strcasecmp(key, "maxsize") == 0) {
-			maxsize = atoi(value);
+		} else if (strcasecmp(key, "maxsize") == 0) {
+			state.curr_max_size = atoi(value);
 			continue;
+		} else if (strcasecmp(key, "scale") == 0) {
+			state.curr_scale = atoi(value);
 		}
 		if (curr_section == SEC_ICON_APP &&
-		    INBOUND(curr_size, min_res, max_res))
-			strcpy(vector_newelem(&theme.apps), line);
+		    is_size_right(min_res, max_res, &state))
+			copy_icon_dir(vector_newelem(&output->apps),
+			              state.curr_size, state.currdir);
 
 		else if (curr_section == SEC_ICON_MIMETYPES &&
-			INBOUND(curr_size, min_res, max_res))
-			strcpy(vector_newelem(&theme.mimes), line);
+		    is_size_right(min_res, max_res, &state))
+			copy_icon_dir(vector_newelem(&output->mimes),
+			              state.curr_size, state.currdir);
 
 		else if (curr_section == SEC_ICON_PLACES &&
-			INBOUND(curr_size, min_res, max_res))
-			strcpy(vector_newelem(&theme.places), line);
+		    is_size_right(min_res, max_res, &state))
+			copy_icon_dir(vector_newelem(&output->places),
+			              state.curr_size, state.currdir);
 
 		else if (curr_section == SEC_ICON_DEVICES &&
-			INBOUND(curr_size, min_res, max_res))
-			strcpy(vector_newelem(&theme.devices), line);
+		    is_size_right(min_res, max_res, &state))
+			copy_icon_dir(vector_newelem(&output->devices),
+			              state.curr_size, state.currdir);
 
 		else if (curr_section == SEC_ICON_STATUS &&
-			INBOUND(curr_size, min_res, max_res))
-			strcpy(vector_newelem(&theme.status), line);
-
+		    is_size_right(min_res, max_res, &state))
+			copy_icon_dir(vector_newelem(&output->status),
+			              state.curr_size, state.currdir);
 	}
 	free(rawline);
+
+	qsort(output->apps.elems, output->apps.len, output->apps.elemsize,
+	      cmp_icon_dir);
+	qsort(output->mimes.elems, output->mimes.len, output->mimes.elemsize,
+	      cmp_icon_dir);
+	qsort(output->places.elems, output->places.len, output->places.elemsize,
+	      cmp_icon_dir);
+	qsort(output->devices.elems, output->devices.len, output->devices.elemsize,
+	      cmp_icon_dir);
+	qsort(output->status.elems, output->status.len, output->status.elemsize,
+	      cmp_icon_dir);
 
 out:
 	closedir(dir);
 }
 
+static bool
+filename_exists(const char *filename,
+		const struct wl_array *handle_pool,
+		const struct wl_array *string_pool)
+{
+	off_t *off;
+	const char *ptr = string_pool->data;
+	wl_array_for_each(off, handle_pool) {
+		const char *path = ptr + *off;
+		if (strstr(path, filename))
+			return true;
+	}
+	return false;
+}
+
+static int
+search_icon_subdir(const char *dir_path,
+		struct wl_array *handle_pool,
+		struct wl_array *string_pool)
+{
+	int count = 0;
+	char filepath[1024];
+	DIR *dir = opendir(dir_path);
+	if (!dir)
+		return 0;
+	for (struct dirent *entry = readdir(dir); entry;
+	     entry = readdir(dir)) {
+		if (!(entry->d_type == DT_REG ||
+		      entry->d_type == DT_LNK) ||
+		    (!is_file_type(entry->d_name, ".svg") &&
+		     !is_file_type(entry->d_name, ".png")))
+			continue;
+		if (filename_exists(entry->d_name, handle_pool, string_pool))
+			continue;
+		if (strlen(entry->d_name) + strlen(dir_path) + 1 >= 1024)
+			continue;
+		strcpy(filepath, dir_path);
+		path_concat(filepath, 1024, 1, entry->d_name);
+		char *tocpy = wl_array_add(string_pool, sizeof(filepath)+1);
+		*(off_t *)wl_array_add(handle_pool, sizeof(off_t)) =
+			(tocpy - (char *)string_pool->data);
+		strcpy(tocpy, filepath);
+		count++;
+	}
+	closedir(dir);
+	return count;
+}
+
+void
+search_icon_imgs(struct wl_array *handle_pool, struct wl_array *string_pool,
+                 const char *themepath, const vector_t *icondir)
+{
+	//handles needs to be initialized
+	char absdir[1024];
+	struct icon_dir *icons = NULL;
+	int count = 0;
+
+	if (strlen(themepath) + MAX_DIR_LEN + 2 >= 1024)
+		return;
+
+	vector_for_each(icons, icondir) {
+		strcpy(absdir, themepath);
+		path_concat(absdir, sizeof(absdir), 1, icons->dir);
+		count += search_icon_subdir(absdir, handle_pool, string_pool);
+	}
+
+}
 
 int main(int argc, char *argv[])
 {
+	struct wl_array string_pool, handle_pool;
+	struct icontheme_dir themedir;
+	/* struct icon_dir *icon = NULL; */
+	/* off_t *off = NULL; */
 
+	search_icon_dirs(&themedir, "/usr/share/icons/Humanity", 31, 128);
+	//print
+	/* vector_for_each(icon, &themedir.apps) */
+	/*	printf("%s\t", icon->dir); */
+	/* printf("\n"); */
+	/* vector_for_each(icon, &themedir.mimes) */
+	/*	printf("%s\t", icon->dir); */
+	/* printf("\n"); */
+	/* vector_for_each(icon, &themedir.places) */
+	/*	printf("%s\t", icon->dir); */
+	/* printf("\n"); */
+	/* vector_for_each(icon, &themedir.devices) */
+	/*	printf("%s\t", icon->dir); */
+	/* printf("\n"); */
+	/* vector_for_each(icon, &themedir.status) */
+	/*	printf("%s\t", icon->dir); */
+	/* printf("\n"); */
+
+	//doing search on
+	vector_t *to_searches[] = {
+		&themedir.apps, &themedir.mimes, &themedir.devices,
+		&themedir.places, &themedir.status
+	};
+	for (int i = 0; i < NUMOF(to_searches); i++) {
+		wl_array_init(&string_pool);
+		wl_array_init(&handle_pool);
+		search_icon_imgs(&handle_pool, &string_pool,
+		                 "/usr/share/icons/Humanity", to_searches[i]);
+		/* wl_array_for_each(off, &handle_pool) { */
+		/*	fprintf(stdout, "%s\n", (char *)string_pool.data + *off); */
+		/* } */
+
+		wl_array_release(&string_pool);
+		wl_array_release(&handle_pool);
+	}
+
+
+
+	icontheme_dir_release(&themedir);
 	return 0;
 }
