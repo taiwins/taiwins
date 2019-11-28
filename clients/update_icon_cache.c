ebfe8afefe8c66c18f445d341c1fe9bfc7731594
Xichen Zhou <xzhou@xeechou.net>
2019-11-28 14:33:08 -0500

    good impl of update icon cache

--- clients/update_icon_cache.c
+++ clients/update_icon_cache.c
@@ -1,8 +1,6 @@
-#include "vector.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
 #include <libgen.h>
@@ -11,6 +9,7 @@
 #include <sys/stat.h>
 #include <linux/limits.h>
 #include <fcntl.h>
+#include <cairo.h>
 
 #include <strops.h>
 #include <os/file.h>
@@ -19,26 +18,19 @@
 
 #include "common.h"
 
-//the struct requires two parts, a image list with name and dimensions, and a
-//tile image.
-
-//it is the updater's responsibility to collect all the images and also quick
-//hash directories(optional), the readers simply reads the cache and provides
-//radix tree to do quick search, another problem here is that is has png/svg/xpm
-
-// then you would have a big ass tile picture that loads everything for you.
-// which we have a base resolution,
 struct icon_cache_option {
 	bool update_all;
 	bool force_update;
 	const char *update_theme;
 	int high_res; //highest resolution to search
 	int low_res; //lowest resolution to search
+	uint32_t update_list;
 };
 
 struct icon_cache_config {
 	char path[256];
-	int res;
+	int high_res;
+	int low_res;
 };
 
 static const char *usage =
@@ -51,160 +43,54 @@ static const char *usage =
 	"  -f, --force\t\t\tIgnore the existing cache\n"
 	"  -h, --hires\t\t\tSpecify the highest resolution to sample, maximum 256, default 128\n"
 	"  -l, --lowres\t\t\tSpecify the lowest resolution to sample, minimum 32, default 32\n"
-	"  -t, --theme\t\t\tSepcify the theme to update\n"
+	"  -t, --theme\t\t\tSepcify the theme to update, default all\n"
+	"  -u, --updates\t\t\tSpecify the update list in (apps,mimes,places,status,devices). default all\n"
 	"\n";
 
 static const char *hicolor_path = "/usr/share/icons/hicolor";
 
 //match those with name
 enum icon_type {
-	ICON_ACTIONS = 0,
-	ICON_ANIMATIONS = 1,
-	ICON_APPS = 2,
-	ICON_CATEGORIES = 3,
-	ICON_DEVICES = 4,
-	ICON_MIMETYPES = 5,
-	ICON_STATUS = 6,
+	ICON_APPS = 1,
+	ICON_MIMETYPES = 2,
+	ICON_PLACES = 4,
+	ICON_STATUS = 8,
+	ICON_DEVICES = 16,
+	ICON_UNKNOWN = 100000,
 };
 
-//obtaining
-static int
-icon_size_cmp(const void *p1, const void *p2)
+static enum icon_type
+icon_type_from_name(const char *name)
 {
-	int p1_resx, p1_resy, p2_resx, p2_resy;
-	sscanf((const char *)p1, "%dx%d", &p1_resx, &p1_resy);
-	sscanf((const char *)p2, "%dx%d", &p2_resx, &p2_resy);
-	return p2_resx - p1_resx;
+	if (!strcmp(name, "apps"))
+		return ICON_APPS;
+	else if (!strcmp(name, "mimes"))
+		return ICON_MIMETYPES;
+	else if (!strcmp(name, "places"))
+		return ICON_PLACES;
+	else if (!strcmp(name, "status"))
+		return ICON_STATUS;
+	else if (!strcmp(name, "devices"))
+		return ICON_DEVICES;
+	else
+		return ICON_UNKNOWN;
 }
 
-static bool
-filename_exists(const char *filename,
-		const struct wl_array *handle_pool,
-		const struct wl_array *string_pool)
+static const char *
+name_from_icon_type(uint32_t type)
 {
-	off_t *off;
-	const char *ptr = string_pool->data;
-	wl_array_for_each(off, handle_pool) {
-		const char *path = ptr + *off;
-		if (strstr(path, filename))
-			return true;
-	}
-	return false;
+	if (type == ICON_APPS)
+		return ".apps.icon.cache";
+	else if (type == ICON_MIMETYPES)
+		return ".mimes.icon.cache";
+	else if (type == ICON_PLACES)
+		return ".places.icon.cache";
+	else if (type == ICON_STATUS)
+		return ".status.icon.cache";
+	else if (type == ICON_DEVICES)
+		return ".devices.icon.cache";
+	return NULL;
 }
-
-static int
-search_icon_dir(const char *dir_path,
-		struct wl_array *handle_pool,
-		struct wl_array *string_pool)
-{
-	int count = 0;
-	DIR *dir = opendir(dir_path);
-
-	if (!dir)
-		return count;
-	for (struct dirent *entry = readdir(dir); entry; entry = readdir(dir)) {
-		char file_path[1024];
-		//TODO, take svg as well.
-		if (!(entry->d_type == DT_REG ||
-		      entry->d_type == DT_LNK) ||
-		    !is_file_type(entry->d_name, ".png"))
-			continue;
-		if (filename_exists(entry->d_name, handle_pool, string_pool))
-			continue;
-		strcpy(file_path, dir_path);
-		//find the same file in the string_pool
-		path_concat(file_path, 1024, 1, entry->d_name);
-		char *tocpy = wl_array_add(string_pool, sizeof(file_path)+1);
-		*(off_t *)wl_array_add(handle_pool, sizeof(off_t)) =
-			(tocpy - (char *)string_pool->data);
-		strcpy(tocpy, file_path);
-		count++;
-	}
-	closedir(dir);
-	return count;
-}
-
-static int
-search_theme(const struct icon_cache_config *config,
-	     struct wl_array *handle_pool,
-	     struct wl_array *str_pool)
-{
-	int count = 0;
-	vector_t resolutions;
-	vector_t hicolor_resolutions;
-	typedef char dir_name_t[256];
-
-	struct {
-		vector_t *res;
-		const char *path;
-	} to_search[] = {
-		{&resolutions, config->path},
-		{&hicolor_resolutions, hicolor_path},
-	};
-
-	for (int i = 0; i < 2; i++) {
-		DIR *dir = NULL;
-		int resx, resy;
-
-		vector_init_zero(to_search[i].res, 256, NULL);
-		if (!to_search[i].path)
-			continue;
-		if (!(dir = opendir(to_search[i].path)))
-			break;
-		for(struct dirent *entry = readdir(dir);
-		    entry; entry = readdir(dir)) {
-			if (sscanf(entry->d_name, "%dx%d", &resx, &resy) == 2 &&
-			    entry->d_type == DT_DIR &&
-			    resx <= config->res) {
-				char *path = vector_newelem(to_search[i].res);
-				strop_ncpy(path, entry->d_name, 256);
-			}
-		}
-		closedir(dir);
-		qsort(to_search[i].res->elems,
-		      to_search[i].res->len,
-		      to_search[i].res->elemsize,
-		      icon_size_cmp);
-	}
-	if (!resolutions.len && !hicolor_resolutions.len)
-		goto out;
-	/* for (int i = 0; i < 2; i++) { */
-	/*	dir_name_t *pos; */
-	/*	vector_for_each(pos, to_search[i].res) */
-	/*		fprintf(stderr, "%s\n", *pos); */
-	/* } */
-
-	for (int i = 0; i < 2; i++) {
-		//search for some dirs and(right now just the app dir)
-		dir_name_t *pos;
-		char path[1024];
-		vector_for_each(pos, to_search[i].res) {
-			strcpy(path, to_search[i].path);
-			path_concat(path, 1024, 2, pos, "apps");
-			if (!is_dir_exist(path))
-				continue;
-			count += search_icon_dir(path, handle_pool, str_pool);
-		}
-	}
-out:
-	vector_destroy(&resolutions);
-	vector_destroy(&hicolor_resolutions);
-	return count;
-}
-
-//decided whether to search svgs, if we search for svgs, include rsvg is a necessary.
-
-static void
-path_to_node(char output[256], const char *input)
-{
-	char *copy = strdup(input);
-	char *base = basename(copy);
-	//remove png at the tail.
-	strop_ncpy(output, base, 256);
-	free(copy);
-}
-
-#include <cairo.h>
 
 static bool
 cache_needs_update(const char *cache_file, const char *theme_path)
@@ -228,6 +114,8 @@ parse_arguments(int argc, char *argv[],
 	option->update_theme = NULL;
 	option->high_res = 128;
 	option->low_res = 32;
+	option->update_list = ICON_APPS | ICON_MIMETYPES |
+		ICON_PLACES | ICON_STATUS | ICON_DEVICES;
 	for (int i = 1; i < argc; i++) {
 		const char *arg = argv[i];
 		if (!strcmp(arg, "--help")) {
@@ -247,6 +135,23 @@ parse_arguments(int argc, char *argv[],
 			   (i+1) < argc) {
 			option->low_res = atoi(argv[i+1]);
 			i++;
+		} else if ((!strcmp(arg, "-u") || !strcmp(arg, "--updates")) &&
+		           (i+1) < argc) {
+			option->update_list = 0;
+			char *save_ptr = NULL, *elem = NULL;
+			enum icon_type type = ICON_UNKNOWN;
+			for (elem = strtok_r(argv[i+1], ",", &save_ptr);
+			     elem; elem = strtok_r(NULL, ",", &save_ptr)) {
+				if ((type = icon_type_from_name(elem)) ==
+				    ICON_UNKNOWN)
+					return false;
+				option->update_list |= type;
+			}
+			if (!option->update_list) {
+				fprintf(stderr, "%s", usage);
+				return false;
+			}
+
 		} else {
 			fprintf(stderr, "%s", usage);
 			return false;
@@ -258,64 +163,149 @@ parse_arguments(int argc, char *argv[],
 	return true;
 }
 
+static bool
+retrieve_themes_to_search(vector_t *theme_lookups,
+                          const struct icon_cache_option *option)
+{
+	vector_init_zero(theme_lookups,
+			 sizeof(struct icon_cache_config),
+			 NULL);
+	if (option->update_theme) {
+		struct icon_cache_config theme_elem = {
+			.high_res =  option->high_res,
+			.low_res = option->low_res
+		};
+
+		path_concat(theme_elem.path, sizeof(theme_elem.path), 2,
+		            "usr/share/icons", option->update_theme);
+		if (!is_dir_exist(theme_elem.path)) {
+			fprintf(stderr, "theme %s not found", option->update_theme);
+			return false;
+		}
+		vector_append(theme_lookups, &theme_elem);
+	} else {  //update all
+		DIR *dir = opendir("/usr/share/icons");
+		for(struct dirent *entry = readdir(dir);
+		    entry; entry = readdir(dir)) {
+			struct icon_cache_config theme_elem = {
+				.high_res =  option->high_res,
+				.low_res = option->low_res
+			};
+
+			if (strcmp(entry->d_name, ".") == 0 ||
+			    strcmp(entry->d_name, "..") == 0)
+				continue;
+			path_concat(theme_elem.path, sizeof(theme_elem.path), 2,
+			            "usr/share/icons", entry->d_name);
+			vector_append(theme_lookups, &theme_elem);
+		}
+		closedir(dir);
+	}
+	return true;
+}
+
+static void
+path_to_node(char output[256], const char *input)
+{
+	char *copy = strdup(input);
+	char *base = basename(copy);
+	*strrchr(base, '.') = 0;
+	strop_ncpy(output, base, 256);
+	free(copy);
+}
+
+static void
+update_theme(const struct icon_cache_config *current,
+             const struct icon_cache_option *option,
+             const struct icontheme_dir *hicolor)
+{
+	char cache_file[PATH_MAX];
+	char path[256];
+	strcpy(path, current->path);
+	char *name = basename(path);
+	struct icontheme_dir theme;
+	const vector_t *dirs[] = {
+		&theme.apps, &theme.mimes, &theme.places,
+		&theme.status, &theme.devices
+	};
+	const vector_t *hdirs[] = {
+		&hicolor->apps, &hicolor->mimes, &hicolor->places,
+		&hicolor->status, &hicolor->devices
+	};
+	icontheme_dir_init(&theme, current->path);
+	search_icon_dirs(&theme, current->low_res-1, current->high_res);
+
+	for (unsigned int i = 0; i < 5; i++) {
+		if (!(1 << i & option->update_list))
+			continue;
+		taiwins_cache_dir(cache_file);
+		strcat(cache_file, "/");
+		strcat(cache_file, name);
+		strcat(cache_file, name_from_icon_type(1 << i));
+
+		if (!cache_needs_update(cache_file, current->path) &&
+		    !option->force_update)
+			continue;
+		//retrieve image files
+		struct wl_array string_pool, handle_pool;
+		wl_array_init(&string_pool);
+		wl_array_init(&handle_pool);
+		search_icon_imgs(&handle_pool, &string_pool, current->path,
+		                 dirs[i]);
+		search_icon_imgs(&handle_pool, &string_pool, hicolor_path,
+		                 hdirs[i]);
+		if (!handle_pool.size)
+			goto skip_writing;
+		//write cache
+		struct image_cache cache =
+			image_cache_from_arrays(&handle_pool, &string_pool,
+			                        path_to_node);
+		int flags = is_file_exist(cache_file) ? O_RDWR : O_RDWR | O_CREAT;
+		int fd = open(cache_file, flags, S_IRUSR | S_IWUSR | S_IRGRP);
+		image_cache_to_fd(&cache, fd);
+#if 0
+		cairo_surface_t *surface =
+			cairo_image_surface_create_for_data(cache.atlas,
+			                                    CAIRO_FORMAT_ARGB32,
+			                                    cache.dimension.w, cache.dimension.h,
+			                                    cache.dimension.w * 4);
+		strcat(cache_file, ".png");
+		cairo_surface_write_to_png(surface, cache_file);
+		cairo_surface_destroy(surface);
+#endif
+
+		close(fd);
+		image_cache_release(&cache);
+	skip_writing:
+		wl_array_release(&handle_pool);
+		wl_array_release(&string_pool);
+	}
+	icontheme_dir_release(&theme);
+}
+
 int
 main(int argc, char *argv[])
 {
-	int fd = -1;
 	vector_t theme_lookups;
 	struct icon_cache_option option;
-	struct icon_cache_config theme, *current = NULL;
-	char cache_home[PATH_MAX];
+	struct icon_cache_config *current = NULL;
 
 	if (!parse_arguments(argc, argv, &option))
 		return -1;
 	if (!create_cache_dir())
 		return -1;
-	vector_init_zero(&theme_lookups,
-			 sizeof(struct icon_cache_config),
-			 NULL);
-	theme.res = option.high_res;
-	if (option.update_theme) {
-		sprintf(theme.path, "%s/%s", "/usr/share/icons", option.update_theme);
-		if (!is_dir_exist(theme.path)) {
-			fprintf(stderr, "theme %s not found", option.update_theme);
-			return -1;
-		}
-		vector_append(&theme_lookups, &theme);
-	} else { /* update all */
-		DIR *dir = opendir("/usr/share/icons");
-		for(struct dirent *entry = readdir(dir);
-		    entry; entry = readdir(dir)) {
-			strcpy(theme.path, "/usr/share/icons/");
-			strcat(theme.path, entry->d_name);
-			vector_append(&theme_lookups, &theme);
-		}
-		closedir(dir);
-	}
+	if (!retrieve_themes_to_search(&theme_lookups, &option))
+		return -1;
+
+	struct icontheme_dir hicolor_theme;
+	icontheme_dir_init(&hicolor_theme, hicolor_path);
+	search_icon_dirs(&hicolor_theme, option.low_res-1, option.high_res);
+
 	//now we just do the config
-	vector_for_each(current, &theme_lookups) {
-		char *name = basename(current->path);
-		taiwins_cache_dir(cache_home);
-		strcat(cache_home, "/");
-		strcat(cache_home, name);
-		strcat(cache_home, ".icon.cache");
-		if (!option.force_update &&
-		    !cache_needs_update(cache_home, current->path))
-			continue;
-		struct wl_array handles, strings;
-		wl_array_init(&handles);
-		wl_array_init(&strings);
-		search_theme(current, &handles, &strings);
-		struct image_cache cache =
-			image_cache_from_arrays(&handles, &strings, path_to_node);
-		int flags = is_file_exist(cache_home) ? O_RDWR : O_RDWR | O_CREAT;
-		fd = open(cache_home, flags, S_IRUSR | S_IWUSR | S_IRGRP);
-		image_cache_to_fd(&cache, fd);
-		wl_array_release(&handles);
-		wl_array_release(&strings);
-		image_cache_release(&cache);
-		close(fd);
-	}
+	vector_for_each(current, &theme_lookups)
+		update_theme(current, &option, &hicolor_theme);
+
+	icontheme_dir_release(&hicolor_theme);
 	vector_destroy(&theme_lookups);
 	return 0;
 }
