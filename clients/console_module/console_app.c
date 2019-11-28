3146ac8040c2d1a63f568e2c3472cc5b1e5e1e84
Xichen Zhou <xzhou@xeechou.net>
2019-11-28 18:33:36 -0500

    console_application

--- clients/console_module/console_app.c
+++ clients/console_module/console_app.c
@@ -1,3 +1,7 @@
+#ifndef _GNU_SOURCE /* for qsort_r */
+#define _GNU_SOURCE
+#endif
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,11 +10,17 @@
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 #include <rax.h>
 #include <desktop_entry.h>
+#include <os/exec.h>
+#include <strops.h>
 #include "console_module.h"
-#include "vector.h"
+#include <vector.h>
+
 
 static inline void
 tolowers(char *line)
@@ -18,31 +28,71 @@ tolowers(char *line)
 	for (; *line; line++) *line = tolower(*line);
 }
 
+void remove_exec_param(char *exec)
+{
+	char *ptr = exec;
+	while (*ptr) {
+		if (*ptr == '%' && !isspace(*(ptr+1)))
+			*ptr = '\0';
+		ptr++;
+	}
+}
 
 static int
 xdg_app_module_exec(struct console_module *module, const char *entry,
 		    char **result)
 {
+	struct xdg_app_entry *app = NULL;
+	char *name = strop_ltrim((char *)entry);
+	char execpy[256];
+	char *argv[128] = {0};
+	int closepid;
+
+	vector_for_each(app, (vector_t *)module->user_data) {
+		if (strcasecmp(name, app->name) == 0)
+			break;
+	}
+	if (!app)
+		return -1;
+	strcpy(execpy, app->exec);
+	remove_exec_param(execpy);
+
+	int argc = 0;
+	char *tok, *saved_ptr = NULL;
+	for (tok = strtok_r(execpy, " ", &saved_ptr); tok;
+	     tok = strtok_r(NULL, " ", &saved_ptr)) {
+		argv[argc] = tok;
+		argc += 1;
+	}
+	fork_exec(argc, argv);
 	return 0;
 }
-
 
 static int
 xdg_app_module_search(struct console_module *module, const char *to_search,
 		      vector_t *result)
 {
-	struct raxIterator iter;
-
-	if (!module->radix)
-		return 0;
+	struct xdg_app_entry *app = NULL;
 	//the search need to done for (strstr(x, to_search) for all x in my vec.)
 	//there should be a effective algorithm for that.
 
 	vector_init_zero(result, sizeof(console_search_entry_t),
 			 free_console_search_entry);
-
-	raxStart(&iter, module->radix);
-
+	//TODO replace this brute force with a more efficient algorithm
+	vector_for_each(app, (vector_t *)module->user_data) {
+		if (strcasestr(app->name, to_search)) {
+			printf("app name: %s, search: %s\n", app->name, to_search);
+			console_search_entry_t *entry =
+				vector_newelem(result);
+			if (strlen(app->name) < 32) {
+				strcpy(entry->sstr, app->name);
+				entry->pstr = NULL;
+			} else
+				entry->pstr = strdup(app->name);
+		}
+	}
+	//now we can have a sort, but it requires edit distance
+	/* qsort_r(result->elems, result->len, result->elemsize, NULL, to_search); */
 	return 0;
 }
 
@@ -50,17 +100,16 @@ xdg_app_module_search(struct console_module *module, const char *to_search,
 static void
 xdg_app_module_init(struct console_module *module)
 {
+	//use signal for this
+	//TODO check signal to terminate children if children terminated
 	vector_t *apps = module->user_data;
 
 	*apps = xdg_apps_gather();
-	module->radix = raxNew();
 	//insert into spaces
 	struct xdg_app_entry *app = NULL;
 	vector_for_each(app, apps) {
 		char *name = strdup(app->name);
 		tolowers(name);
-		raxInsert(module->radix, (unsigned char *)name, strlen(name),
-			  app, NULL);
 		free(name);
 	}
 }
@@ -69,8 +118,8 @@ xdg_app_module_init(struct console_module *module)
 static void
 xdg_app_module_destroy(struct console_module *module)
 {
-	raxFree(module->radix);
-	vector_destroy(module->user_data);
+	vector_t *apps = module->user_data;
+	vector_destroy(apps);
 }
 
 
@@ -82,6 +131,6 @@ struct console_module app_module = {
 	.search = xdg_app_module_search,
 	.init_hook = xdg_app_module_init,
 	.destroy_hook = xdg_app_module_destroy,
-	.support_cache = true,
+	.support_cache = false,
 	.user_data = &xdg_app_vector,
 };
