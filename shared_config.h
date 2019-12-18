b2d74c30fb03b7d5ccb8f24ad629f7d407e0d364
Xichen Zhou <xzhou@xeechou.net>
2019-12-18 14:32:12 -0500

    add missing copyright notice in protocol

--- shared_config.h
+++ shared_config.h
@@ -1,13 +1,26 @@
-/*******************************************************************
+/*
+ * shared_config.h - taiwins shared configuration for server and clients
  *
- * this file servers the purpose of (default)unified configuration
- * shared among server and client
+ * Copyright (c) 2019 Xichen Zhou
  *
- ******************************************************************/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 #ifndef TW_SHARED_CONFIG_H
 #define TW_SHARED_CONFIG_H
-
 
 #include <stdint.h>
 #include <stdbool.h>
