a8cb5415fe21cec9d6403e08f1df2be79ff0ae10
Xichen Zhou <xzhou@xeechou.net>
2019-12-18 14:24:04 -0500

    update license

--- clients/shell_bg.c
+++ clients/shell_bg.c
@@ -1,5 +1,25 @@
-#include "shell.h"
+/*
+ * shell_bg.c - taiwins client shell background implementation
+ *
+ * Copyright (c) 2019 Xichen Zhou
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
+#include "shell.h"
 
 static bool
 shell_background_start_menu(struct app_surface *surf, const struct app_event *e)
--- clients/widget_lua.c
+++ clients/widget_lua.c
@@ -1,8 +1,22 @@
 /*
- * LUA-Nuklear - MIT licensed; no warranty implied; use at your own risk.
- * authored from 2015-2016 by Micha Mettke, orignally LOVE-Nuklear
- * adapted to LOVE in 2016 by Kevin Harrison
- * adopted to taiwins in 2019 by Sichem
+ * widget_lua.c - taiwins client shell widget lua binding
+ *
+ * Copyright (c) 2019 Xichen Zhou
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 
 #ifndef NK_LUA_WIDGET_H
