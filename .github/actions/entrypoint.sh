510b0cfd38fefa4fd9536a82b661b0600527973a
xeechou <xzhou@xeechou.net>
2020-03-02 00:01:15 -0500

    add vulkan-icd-loader

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Syu --needed --noconfirm weston libxkbcommon pixman mesa wayland libglvnd vulkan-headers wayland-protocols fontconfig freetype2 librsvg lua cairo pam doxygen
+pacman -Syu --needed --noconfirm weston libxkbcommon pixman mesa wayland libglvnd vulkan-headers vulkan-icd-loader wayland-protocols fontconfig freetype2 librsvg lua cairo pam doxygen
 pacman -Syu --needed --noconfirm cmake make gcc clang musl git meson ninja rsync pkgconfig
 # build step
 rm -rf build && mkdir -p build && cd build && cmake .. && make
