bef9e04c333b1ebc60ae6e0fa08a75226cfa41cb
xeechou <xeechou@gmail.com>
2020-03-08 13:53:33 -0400

    add gst depedencies for ci

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pacman -Syu --needed --noconfirm weston libxkbcommon pixman mesa wayland libglvnd vulkan-headers vulkan-icd-loader wayland-protocols fontconfig freetype2 librsvg lua cairo pam doxygen
+pacman -Syu --needed --noconfirm weston libxkbcommon pixman mesa wayland  wayland-protocols fontconfig freetype2 librsvg lua cairo pam doxygen
+pacman -Syu --needed --noconfirm libglvnd vulkan-headers vulkan-icd-loader
+pacman -Syu --needed --noconfirm gstreamer gst-plugins-base gst-plugins-base-libs
 pacman -Syu --needed --noconfirm cmake make gcc clang musl git meson ninja rsync pkgconfig
 # build step
 rm -rf build && mkdir -p build && cd build && cmake .. && make
