fd1fcbd71cdcb65a0375450ba24d424682b17e03
Xichen Zhou <xzhou@xeechou.net>
2020-10-13 11:38:55 -0400

    ctypes: add ctypes as toplevel subprojects so we don't need to wrap it anymore
    Signed-off-by: Xichen Zhou <xzhou@xeechou.net>

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -5,5 +5,4 @@ pacman -Syu --needed --noconfirm libglvnd vulkan-headers vulkan-icd-loader
 pacman -Syu --needed --noconfirm gstreamer gst-plugins-base gst-plugins-base-libs
 pacman -Syu --needed --noconfirm cmake make gcc clang musl git meson ninja rsync pkgconfig
 # build step
-meson wrap promote subprojects/twclient/subprojects/ctypes
 rm -rf build && meson -Dx11-backend=enabled build && ninja -C build
