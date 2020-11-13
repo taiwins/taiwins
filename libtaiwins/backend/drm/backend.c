13b4e47c0f84657946c84ee53dc3d82fd25832e5
Xichen Zhou <xzhou@xeechou.net>
2020-11-13 11:56:34 +0800

    drm: init drm resources
    Signed-off-by: Xichen Zhou <xzhou@xeechou.net>

--- libtaiwins/backend/drm/backend.c
+++ libtaiwins/backend/drm/backend.c
@@ -19,33 +19,79 @@
  *
  */
 
-#include "options.h"
-
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
-#include <wayland-util.h>
+#include <unistd.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>
-#include <libudev.h>
 #include <sys/types.h>
-#include <wayland-server-core.h>
+#include <wayland-server.h>
 
 #include <taiwins/backend.h>
 #include <taiwins/backend_drm.h>
 #include <taiwins/objects/logger.h>
 #include <taiwins/objects/utils.h>
+#include <taiwins/objects/egl.h>
 #include <taiwins/render_context.h>
 
 #include "login/login.h"
+#include "internal.h"
 
-struct tw_drm_backend {
-	struct tw_backend base;
-	struct tw_login *login;
-	int gpu_id;
+static bool
+drm_backend_start(struct tw_backend *backend, struct tw_render_context *ctx)
+{
+	struct tw_drm_display *output;
+	struct tw_drm_backend *drm = wl_container_of(backend, drm, base);
 
-	struct wl_listener display_destroy;
+	if (tw_drm_init_gbm(drm) == false)
+		return false;
+
+	wl_list_for_each(output, &drm->base.outputs, output.device.link)
+		tw_drm_display_start(output);
+	return true;
+}
+
+static const struct tw_egl_options *
+drm_gen_egl_params(struct tw_backend *backend)
+{
+	struct tw_drm_backend *drm = wl_container_of(backend, drm, base);
+	//TODO, we just use argb8888 for now
+	static struct tw_egl_options egl_opts = {
+		.platform = EGL_PLATFORM_GBM_KHR,
+		.visual_id = GBM_FORMAT_ARGB8888,
+	};
+	static const EGLint egl_config_attribs[] = {
+		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+		EGL_BLUE_SIZE, 1,
+		EGL_GREEN_SIZE, 1,
+		EGL_RED_SIZE, 1,
+		EGL_ALPHA_SIZE, 0,
+		EGL_NONE,
+	};
+	egl_opts.context_attribs = egl_config_attribs;
+	egl_opts.native_display = drm->gbm.dev;
+	return &egl_opts;
+}
+
+static const struct tw_backend_impl drm_impl = {
+	.start = drm_backend_start,
+	.gen_egl_params = drm_gen_egl_params,
 };
+
+static void
+drm_backend_stop(struct tw_drm_backend *drm)
+{
+	struct tw_drm_display *output, *tmp_output;
+
+	wl_list_for_each_safe(output, tmp_output, &drm->base.outputs,
+	                      output.device.link)
+		tw_drm_display_remove(output);
+	wl_signal_emit(&drm->base.events.stop, &drm->base);
+	wl_list_remove(&drm->base.render_context_destroy.link);
+	drm->base.ctx = NULL;
+}
 
 static void
 drm_backend_destroy(struct tw_drm_backend *drm)
@@ -54,7 +100,27 @@ drm_backend_destroy(struct tw_drm_backend *drm)
 	if (drm->base.ctx)
 		tw_render_context_destroy(drm->base.ctx);
 	tw_login_destroy(drm->login);
+	close(drm->gpu_fd);
 	free(drm);
+}
+
+static void
+notify_drm_login_state(struct wl_listener *listener, void *data)
+{
+	struct tw_drm_backend *drm =
+		wl_container_of(listener, drm, login_listener);
+	struct tw_login *login = data;
+
+	assert(login == drm->login);
+	//TODO
+}
+
+static void
+notify_drm_render_context_destroy(struct wl_listener *listener, void *data)
+{
+	struct tw_drm_backend *drm =
+		wl_container_of(listener, drm, base.render_context_destroy);
+	drm_backend_stop(drm);
 }
 
 static void
@@ -66,32 +132,59 @@ notify_drm_display_destroy(struct wl_listener *listener, void *data)
 	drm_backend_destroy(drm);
 }
 
-static const struct tw_backend_impl drm_impl = {
-
-};
-
 struct tw_backend *
 tw_drm_backend_create(struct wl_display *display)
 {
 	struct tw_drm_backend *drm = calloc(1, sizeof(*drm));
+	struct wl_event_loop *loop = wl_display_get_event_loop(display);
+	struct wl_event_source *source;
 
 	if (!drm)
 		return NULL;
 	tw_backend_init(&drm->base);
 	drm->base.impl = &drm_impl;
+	drm->crtc_mask = 0;
 
 	drm->login = tw_login_create(display);
 	if (!(drm->login))
 		goto err_login;
 
-	drm->gpu_id = tw_login_find_primary_gpu(drm->login);
-	if (drm->gpu_id < 0)
+	drm->gpu_fd = tw_login_find_primary_gpu(drm->login);
+	if (drm->gpu_fd < 0)
 		goto err_gpu_fd;
+	tw_drm_print_info(drm->gpu_fd);
 
+	if (!tw_drm_check_features(drm)) {
+		tw_logl_level(TW_LOG_ERRO, "Failed to check drm features");
+		goto err_features;
+	}
+
+	if (!tw_drm_check_resources(drm)) {
+		tw_logl_level(TW_LOG_ERRO, "Failed to query drm resources");
+		goto err_resource;
+	}
+
+	source = wl_event_loop_add_fd(loop, drm->gpu_fd, WL_EVENT_READABLE,
+	                              tw_drm_handle_drm_event, drm);
+	if (!source) {
+		tw_logl_level(TW_LOG_ERRO, "Failed to monitor drm events");
+		goto err_event;
+	}
+
+	//add listeners
+	tw_signal_setup_listener(&drm->login->events.attributes_change,
+	                         &drm->login_listener,
+	                         notify_drm_login_state);
 	tw_set_display_destroy_listener(display, &drm->display_destroy,
 	                                notify_drm_display_destroy);
+	drm->base.render_context_destroy.notify =
+		notify_drm_render_context_destroy;
 
 	return &drm->base;
+err_event:
+err_resource:
+err_features:
+	close(drm->gpu_fd);
 
 err_gpu_fd:
 	tw_login_destroy(drm->login);
---
+++ libtaiwins/backend/drm/internal.h
@@ -0,0 +1,187 @@
+/*
+ * internal.h - taiwins server drm backend internal header
+ *
+ * Copyright (c) 2020 Xichen Zhou
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef TW_DRM_INTERNAL_H
+#define TW_DRM_INTERNAL_H
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <stdint.h>
+#include <xf86drm.h>
+#include <xf86drmMode.h>
+#include <gbm.h>
+#include <wayland-server.h>
+#include <taiwins/backend_drm.h>
+#include <taiwins/render_output.h>
+#include <taiwins/objects/plane.h>
+#include <taiwins/objects/drm_formats.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+enum tw_drm_features {
+	TW_DRM_CAP_ATOMIC = 1 << 0,
+	TW_DRM_CAP_PRIME = 1 << 1,
+	TW_DRM_CAP_MODIFIERS = 1 << 2,
+	TW_DRM_CAP_DUMPBUFFER = 1 << 3,
+};
+
+enum tw_drm_plane_type {
+	TW_DRM_PLANE_MAJOR,
+	TW_DRM_PLANE_OVERLAY,
+	TW_DRM_PLANE_CURSOR,
+};
+
+struct tw_drm_display;
+
+struct tw_drm_plane {
+	struct tw_plane base;
+	uint32_t id; /**< drm plane id */
+	uint32_t crtc_mask;
+	enum tw_drm_plane_type type;
+
+	struct tw_drm_formats formats;
+};
+
+struct tw_drm_crtc {
+	int id, idx;
+	/** occupied by display */
+	struct tw_drm_display *display;
+	struct wl_list link; /** drm->crtc_list */
+};
+
+struct tw_drm_display {
+	struct tw_render_output output;
+	struct tw_drm_backend *drm;
+	int conn_id, crtc_mask;
+	uint32_t planes; /**< TODO possible planes used by display */
+	/** output has at least one primary plane */
+	struct tw_drm_plane *primary_plane;
+	struct tw_drm_crtc *crtc;
+
+	struct {
+		//TODO maybe we should gamma info for all channels
+		int max_brightness;
+		int brightness;
+	} backlight;
+
+	struct {
+		bool connected;
+		int crtc_id; /* crtc_id read from connector, may not work */
+		struct tw_output_device_mode inherited_mode;
+	} status;
+
+	/** we need a plane to create data on */
+	struct {
+		struct gbm_surface *gbm;
+	} gbm_surface;
+
+};
+
+/**
+ * @brief drm backend datasheet
+ *
+ * We need the backend to support multiple allocation APIs(gbm or eglstream),
+ * multiple rendering API (egl or vulkan).
+ * commit routine (atomic or legacy)
+ */
+struct tw_drm_backend {
+	struct tw_backend base;
+	struct tw_login *login;
+
+	//The data represents a GPU, what about secondary GPUs?
+	int gpu_fd;
+	enum tw_drm_features feats;
+	const struct tw_drm_interface *iface;
+	struct {
+		int max_width, max_height;
+		int min_width, min_height;
+	} limits;
+
+	struct tw_drm_crtc crtcs[32];
+	uint32_t crtc_mask;
+	struct wl_list crtc_list;
+
+	uint32_t plane_mask;
+	struct tw_drm_plane planes[32];
+	struct wl_list plane_list;
+
+	union {
+		struct {
+			struct gbm_device *dev;
+			uint32_t visual_id;
+		} gbm;
+		struct {
+			EGLDeviceEXT egldev;
+		} eglstream;
+	};
+
+	struct wl_listener display_destroy;
+	struct wl_listener login_listener;
+};
+
+void
+tw_drm_print_info(int fd);
+
+bool
+tw_drm_init_gbm(struct tw_drm_backend *drm);
+
+int
+tw_drm_handle_drm_event(int fd, uint32_t mask, void *data);
+
+bool
+tw_drm_check_features(struct tw_drm_backend *drm);
+
+bool
+tw_drm_check_resources(struct tw_drm_backend *drm);
+
+bool
+tw_drm_plane_init(struct tw_drm_plane *plane, int fd,
+                  drmModePlane *drm_plane);
+void
+tw_drm_plane_fini(struct tw_drm_plane *plane);
+
+void
+tw_drm_display_start(struct tw_drm_display *display);
+
+bool
+tw_drm_display_start_gbm(struct tw_drm_display *display);
+
+void
+tw_drm_display_remove(struct tw_drm_display *display);
+
+struct tw_drm_display *
+tw_drm_display_find_create(struct tw_drm_backend *drm, drmModeConnector *conn);
+
+bool
+tw_drm_get_property(int fd, uint32_t obj_id, uint32_t obj_type,
+                    const char *prop_name, uint64_t *value);
+struct gbm_surface *
+tw_drm_create_gbm_surface(struct gbm_device *dev, uint32_t w, uint32_t h,
+                          uint32_t format, int n_mods, uint64_t *modifiers,
+                          uint32_t flags);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif /* EOF */
