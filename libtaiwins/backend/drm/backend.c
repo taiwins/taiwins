d684b21f83f880ef75bff7ab9d0c9577fbe974a3
Xichen Zhou <xzhou@xeechou.net>
2020-11-14 09:47:58 +0800

    tw_drm_gpu: add tw_drm_gpu type
    using `tw_drm_gpu` type to replace global drm device. The PRIME GPU would not
    work at the moment, `drmModeGetResources` would return 0.
    
    Signed-off-by: Xichen Zhou <xzhou@xeechou.net>

--- libtaiwins/backend/drm/backend.c
+++ libtaiwins/backend/drm/backend.c
@@ -44,9 +44,6 @@ drm_backend_start(struct tw_backend *backend, struct tw_render_context *ctx)
 	struct tw_drm_display *output;
 	struct tw_drm_backend *drm = wl_container_of(backend, drm, base);
 
-	if (tw_drm_init_gbm(drm) == false)
-		return false;
-
 	wl_list_for_each(output, &drm->base.outputs, output.device.link)
 		tw_drm_display_start(output);
 	return true;
@@ -56,10 +53,10 @@ static const struct tw_egl_options *
 drm_gen_egl_params(struct tw_backend *backend)
 {
 	struct tw_drm_backend *drm = wl_container_of(backend, drm, base);
-	//TODO, we just use argb8888 for now
+	struct tw_drm_gpu *boot_gpu = drm->boot_gpu;
+
 	static struct tw_egl_options egl_opts = {
 		.platform = EGL_PLATFORM_GBM_KHR,
-		.visual_id = GBM_FORMAT_ARGB8888,
 	};
 	static const EGLint egl_config_attribs[] = {
 		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
@@ -71,7 +68,8 @@ drm_gen_egl_params(struct tw_backend *backend)
 		EGL_NONE,
 	};
 	egl_opts.context_attribs = egl_config_attribs;
-	egl_opts.native_display = drm->gbm.dev;
+	egl_opts.visual_id = boot_gpu->gbm.visual_id;
+	egl_opts.native_display = boot_gpu->gbm.dev;
 	return &egl_opts;
 }
 
@@ -93,14 +91,29 @@ drm_backend_stop(struct tw_drm_backend *drm)
 	drm->base.ctx = NULL;
 }
 
+static inline void
+drm_backend_release_gpus(struct tw_drm_backend *drm)
+{
+	struct tw_drm_gpu *gpu;
+	wl_array_for_each(gpu, &drm->gpus) {
+		// are releasing gpu resources here but the outputs are
+		// destroyed already in `drm_backend_stop`.
+		wl_event_source_remove(gpu->event);
+		tw_drm_free_gpu_resources(gpu);
+		tw_drm_fini_gpu_gbm(gpu);
+		tw_login_close(drm->login, gpu->gpu_fd);
+	}
+	wl_array_release(&drm->gpus);
+}
+
 static void
 drm_backend_destroy(struct tw_drm_backend *drm)
 {
 	wl_signal_emit(&drm->base.events.destroy, &drm->base);
 	if (drm->base.ctx)
 		tw_render_context_destroy(drm->base.ctx);
+	drm_backend_release_gpus(drm);
 	tw_login_destroy(drm->login);
-	close(drm->gpu_fd);
 	free(drm);
 }
 
@@ -132,44 +145,117 @@ notify_drm_display_destroy(struct wl_listener *listener, void *data)
 	drm_backend_destroy(drm);
 }
 
+static bool
+drm_backend_init_gpu(struct tw_drm_gpu *gpu, struct tw_login_gpu *login_gpu,
+                     struct tw_drm_backend *drm)
+{
+	struct wl_event_loop *loop = wl_display_get_event_loop(drm->display);
+
+	gpu->drm = drm;
+	gpu->gpu_fd = login_gpu->fd;
+	gpu->sysnum = login_gpu->sysnum;
+	gpu->boot_vga = login_gpu->boot_vga;
+	gpu->crtc_mask = 0;
+	wl_list_init(&gpu->crtc_list);
+	wl_list_init(&gpu->plane_list);
+
+	if (!tw_drm_check_gpu_features(gpu)) {
+		tw_logl_level(TW_LOG_ERRO, "Failed to check features "
+		              "for gpu-%d", gpu->gpu_fd);
+		goto err_features;
+	}
+	if (!tw_drm_check_gpu_resources(gpu)) {
+		tw_logl_level(TW_LOG_ERRO, "Failed to query drm"
+		              "resources for gpu-%d", gpu->gpu_fd);
+		goto err_resources;
+	}
+	//TODO: having a general handler
+	if (!tw_drm_init_gpu_gbm(gpu))
+		goto err_allocator;
+
+	gpu->event = wl_event_loop_add_fd(loop, gpu->gpu_fd,
+	                                  WL_EVENT_READABLE,
+	                                  tw_drm_handle_drm_event,
+	                                  drm);
+	if (!gpu->event) {
+		tw_logl_level(TW_LOG_ERRO, "Failed to monitor events "
+		              "for gpu-%d", gpu->gpu_fd);
+		goto err_event;
+	}
+	gpu->activated = true;
+	return true;
+
+err_event:
+err_allocator:
+	tw_drm_free_gpu_resources(gpu);
+err_resources:
+err_features:
+	gpu->activated = false;
+	return false;
+}
+
+static bool
+drm_backend_collect_gpus(struct tw_drm_backend *drm)
+{
+	int ngpus = 0;
+	struct tw_login_gpu login_gpus[16] = {0};
+	struct tw_drm_gpu *gpus = NULL;
+
+	wl_array_init(&drm->gpus);
+
+	ngpus = tw_login_find_gpus(drm->login, 16, login_gpus);
+	if (ngpus <= 0)
+		return false;
+
+	gpus = wl_array_add(&drm->gpus, ngpus * sizeof(struct tw_drm_gpu));
+	if (!gpus)
+		goto err_array;
+
+	for (int i = 0; i < ngpus; i++)
+		drm_backend_init_gpu(&gpus[i], &login_gpus[i], drm);
+	//check if boot_vga works
+	for (int i = 0; i < ngpus; i++)
+		if (gpus[i].boot_vga == true && gpus[i].activated == false)
+			return false;
+	return true;
+
+err_array:
+	for (int i = 0; i < ngpus; i++)
+		tw_login_close(drm->login, login_gpus[i].fd);
+	return false;
+}
+
+static inline struct tw_drm_gpu *
+drm_backend_get_boot_gpu(struct tw_drm_backend *drm)
+{
+	struct tw_drm_gpu *gpu;
+
+	wl_array_for_each(gpu, &drm->gpus)
+		if (gpu->boot_vga)
+			return gpu;
+	return NULL;
+}
+
 struct tw_backend *
 tw_drm_backend_create(struct wl_display *display)
 {
 	struct tw_drm_backend *drm = calloc(1, sizeof(*drm));
-	struct wl_event_loop *loop = wl_display_get_event_loop(display);
-	struct wl_event_source *source;
 
 	if (!drm)
 		return NULL;
 	tw_backend_init(&drm->base);
+	drm->display = display;
 	drm->base.impl = &drm_impl;
-	drm->crtc_mask = 0;
+	wl_array_init(&drm->gpus);
 
 	drm->login = tw_login_create(display);
 	if (!(drm->login))
 		goto err_login;
-
-	drm->gpu_fd = tw_login_find_primary_gpu(drm->login);
-	if (drm->gpu_fd < 0)
-		goto err_gpu_fd;
-	tw_drm_print_info(drm->gpu_fd);
-
-	if (!tw_drm_check_features(drm)) {
-		tw_logl_level(TW_LOG_ERRO, "Failed to check drm features");
-		goto err_features;
-	}
-
-	if (!tw_drm_check_resources(drm)) {
-		tw_logl_level(TW_LOG_ERRO, "Failed to query drm resources");
-		goto err_resource;
-	}
-
-	source = wl_event_loop_add_fd(loop, drm->gpu_fd, WL_EVENT_READABLE,
-	                              tw_drm_handle_drm_event, drm);
-	if (!source) {
-		tw_logl_level(TW_LOG_ERRO, "Failed to monitor drm events");
-		goto err_event;
-	}
+	if (!drm_backend_collect_gpus(drm))
+		goto err_collect_gpus;
+	drm->boot_gpu = drm_backend_get_boot_gpu(drm);
+	if (!drm->boot_gpu)
+		goto err_boot_gpu;
 
 	//add listeners
 	tw_signal_setup_listener(&drm->login->events.attributes_change,
@@ -181,15 +267,11 @@ tw_drm_backend_create(struct wl_display *display)
 		notify_drm_render_context_destroy;
 
 	return &drm->base;
-err_event:
-err_resource:
-err_features:
-	close(drm->gpu_fd);
 
-err_gpu_fd:
-	tw_login_destroy(drm->login);
+err_boot_gpu:
+err_collect_gpus:
 err_login:
-	free(drm);
+	drm_backend_destroy(drm);
 	return NULL;
 }
 
