5e65a878fcf3b5aa09b6fbb52298ef201fb76404
Xichen Zhou <xzhou@xeechou.net>
2020-12-15 16:10:40 +0800

    render-output: Adapting new repaint model on headless/wayland/x11 backend.
    Signed-off-by: Xichen Zhou <xzhou@xeechou.net>

--- libtaiwins/backend/headless.c
+++ libtaiwins/backend/headless.c
@@ -104,6 +104,7 @@ notify_output_commit(struct wl_listener *listener, void *data)
 	struct tw_headless_output *output =
 		wl_container_of(listener, output, present_listener);
 	tw_output_device_present(&output->output.device, NULL);
+	tw_render_output_clean_maybe(&output->output);
 }
 
 static bool
