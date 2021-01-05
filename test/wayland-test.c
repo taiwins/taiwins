cc824f61efff9aa6fc84eba611da7d6e374f0e36
Xichen Zhou <xzhou@xeechou.net>
2021-01-05 12:49:51 +0800

    test: adding two surfaces in wayland test for testing
    Signed-off-by: Xichen Zhou <xzhou@xeechou.net>

--- test/wayland-test.c
+++ test/wayland-test.c
@@ -7,10 +7,13 @@
 #include <wayland-server-core.h>
 #include <taiwins/objects/logger.h>
 #include <taiwins/objects/egl.h>
+#include <taiwins/objects/profiler.h>
 #include <taiwins/render_context.h>
 #include <taiwins/render_pipeline.h>
 #include <taiwins/render_output.h>
 #include <taiwins/backend_wayland.h>
+#include <taiwins/engine.h>
+#include <taiwins/xdg.h>
 
 #include <wayland-util.h>
 
@@ -38,7 +41,7 @@ dummy_destroy(struct tw_render_pipeline *pipeline)
 	tw_render_pipeline_fini(pipeline);
 }
 
-static struct tw_render_pipeline dummy_pipeline = {
+struct tw_render_pipeline dummy_pipeline = {
 	.impl.repaint_output = dummy_repaint,
 	.impl.destroy = dummy_destroy,
 };
@@ -63,22 +66,36 @@ int main(int argc, char *argv[])
 	display = wl_display_create();
 	if (!display)
 		return EXIT_FAILURE;
+	tw_profiler_open(display, "/tmp/drm-profiler.json");
+
 	loop = wl_display_get_event_loop(display);
 	struct wl_event_source *sigint =
 		wl_event_loop_add_signal(loop, SIGINT,
 		                         tw_term_on_signal, display);
-
 	struct tw_backend *backend =
 		tw_wl_backend_create(display, getenv("WAYLAND_DISPLAY"));
+	struct tw_engine *engine =
+		tw_engine_create_global(display, backend);
+	struct tw_xdg *xdg =
+		tw_xdg_create_global(display, NULL, engine);
 	if (!backend)
 		goto err;
 	const struct tw_egl_options *opts =
 		tw_backend_get_egl_params(backend);
 	struct tw_render_context *ctx =
 		tw_render_context_create_egl(display, opts);
+	if (!tw_wl_backend_new_output(backend, 1000, 1000))
+		goto err;
+	if (!tw_wl_backend_new_output(backend, 900, 900))
+		goto err;
+	(void)xdg;
 
-	tw_render_pipeline_init(&dummy_pipeline, "dummy_pipeline", ctx);
-	wl_list_insert(ctx->pipelines.next, &dummy_pipeline.link);
+	wl_display_add_socket_auto(display);
+
+	struct tw_render_pipeline *pipeline =
+		tw_egl_render_pipeline_create_default(ctx,
+		                                      &engine->layers_manager);
+	wl_list_insert(ctx->pipelines.next, &pipeline->link);
 
 	tw_backend_start(backend, ctx);
 
@@ -86,6 +103,7 @@ int main(int argc, char *argv[])
 	wl_event_source_remove(sigint);
 
 err:
+	tw_profiler_close();
 	wl_display_destroy(display);
 	return EXIT_FAILURE;
 }
