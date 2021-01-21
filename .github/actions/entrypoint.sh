1e9f4435ba7b5b944bd0472abd69bf3697a3fa9e
Xichen Zhou <xzhou@xeechou.net>
2021-01-21 23:09:14 +0800

    CI: configure xwayland for CI and test
    Signed-off-by: Xichen Zhou <xzhou@xeechou.net>

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -5,4 +5,4 @@ pacman -Syu --needed --noconfirm libglvnd vulkan-headers vulkan-icd-loader
 pacman -Syu --needed --noconfirm gstreamer gst-plugins-base gst-plugins-base-libs
 pacman -Syu --needed --noconfirm cmake make gcc clang musl git meson ninja rsync pkgconfig
 # build step
-rm -rf build && meson -Dx11-backend=enabled build && ninja -C build
+rm -rf build && meson -Dx11-backend=enabled -Dxwayland=enabled build && ninja -C build
